bae591ae13400b4ab0a7e7b0b13f65e422200797
Viktoria Mashenskaya <v.mashenskaia@infusion.game>
2024-11-19 15:45:10 +0300

    Asset path and provider

---
+++ Assets/Scripts/Infrastructure/AssetManegement/AssetPath.cs
@@ -0,0 +1,8 @@
+namespace Infrastructure.AssetManegement
+{
+    public static class AssetPath
+    {
+        public const string Hero = "Hero";
+        public const string Hud = "Hud";
+    }
+}
---
+++ Assets/Scripts/Infrastructure/AssetManegement/AssetProvider.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Infrastructure.AssetManegement
+{
+    public class AssetProvider : IAssetProvider
+    {
+        public GameObject Instantiate(string path, Vector3 at)
+        {
+            var prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab, at, Quaternion.identity);
+        }
+
+        public GameObject Instantiate(string path)
+        {
+            var prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab);
+        }
+    }
+}
